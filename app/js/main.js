d29ea418e04ddf05be00fdd1e5100f6d56dd215c
alekscript <piatopal.alex@gmail.com>
2022-03-27 15:17:55 +0300

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,13 @@ $(function(){
         autoplaySpeed:5000
        }
    );
+
+   $(".star").rateYo({
+    starWidth: "16px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
+
+
 });
