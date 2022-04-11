bda4820e7dc40849497f37c3890e36f967afb8c9
Aleksandr Piatopal <piatopal.alex@gmail.com>
2022-04-11 08:25:38 +0300

    add shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,65 @@
-$(function(){
-   $('.top-slider__inner').slick(
-       {
-        dots: true,
-        arrows:false,
-        fade:true,
-        autoplay:true,
-        autoplaySpeed:5000
-       }
-   );
+$(function (){
+  $('.shop-content__filter-btn').on('click', function(){
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
 
-   $(".star").rateYo({
+  $('.button-list').on('click', function(){
+    $('.product-item').addClass('product-item--list');
+  });
+
+  $('.button-grid').on('click', function(){
+    $('.product-item').removeClass('product-item--list');
+  });
+});
+
+$(function () {
+  $('.select-styler').styler();
+});
+
+$(function(){
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function(data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function(data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+});
+
+$(function (){
+  $(".star").rateYo({
     starWidth: "16px",
     normalFill: "#ccccce",
     ratedFill: "#ffc35b",
     readOnly: true
   });
+});
 
+$(function (){
+  $('.top-slider__inner').slick(
+    {
+      dots: true,
+      arrows: false,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 5000
+    });
+});
+
+
+
+
+
+
+
+
+$(function (){
   function getTimeRemaining(endtime) {
     var t = Date.parse(endtime) - Date.parse(new Date());
     var seconds = Math.floor((t / 1000) % 60);
@@ -30,32 +74,32 @@ $(function(){
       'seconds': seconds
     };
   }
-  
+
   function initializeClock(id, endtime) {
     var clock = document.querySelector('.promo-clock');
     var daysSpan = clock.querySelector('.promo__days');
     var hoursSpan = clock.querySelector('.promo__hours');
     var minutesSpan = clock.querySelector('.promo__minutes');
     var secondsSpan = clock.querySelector('.promo__seconds');
-  
+
     function updateClock() {
       var t = getTimeRemaining(endtime);
-  
+
       daysSpan.innerHTML = t.days;
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
     }
-  
+
     updateClock();
     var timeinterval = setInterval(updateClock, 1000);
   }
-  
-  var deadline= $('.promo-clock').attr('data-time'); //for Ukraine
-//   var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+
+  var deadline = $('.promo-clock').attr('data-time'); //for Ukraine
+  //   var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
   initializeClock('.promo-clock', deadline);
 });
